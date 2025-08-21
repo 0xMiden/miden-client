f8e09961b308ddda87c9010c6d1539c9cc4f5ef1
SantiagoPittella <pittellasantiago@gmail.com>
2025-08-21 13:02:33 -0300

    review: remove tests/ dir

--- bin/integration-tests/src/main.rs
+++ bin/integration-tests/src/main.rs
@@ -6,12 +6,14 @@ use miden_client::rpc::Endpoint;
 use miden_client::testing::config::ClientConfig;
 use url::Url;
 
-use crate::tests::client::*;
-use crate::tests::custom_transaction::*;
-use crate::tests::fpi::*;
-use crate::tests::network_transaction::*;
-use crate::tests::onchain::*;
-use crate::tests::swap_transaction::*;
+use crate::tests::{
+    client,
+    custom_transaction,
+    fpi,
+    network_transaction,
+    onchain,
+    swap_transaction,
+};
 
 mod tests;
 
@@ -84,115 +86,163 @@ async fn run_tests(client_config: &ClientConfig) {
     // CLIENT
     run_test(
         "client_builder_initializes_client_with_endpoint",
-        client_builder_initializes_client_with_endpoint,
+        client::client_builder_initializes_client_with_endpoint,
         &failed_tests,
         client_config,
     )
     .await;
     run_test(
         "multiple_tx_on_same_block",
-        multiple_tx_on_same_block,
+        client::multiple_tx_on_same_block,
         &failed_tests,
         client_config,
     )
     .await;
-    run_test("import_expected_notes", import_expected_notes, &failed_tests, client_config).await;
+    run_test(
+        "import_expected_notes",
+        client::import_expected_notes,
+        &failed_tests,
+        client_config,
+    )
+    .await;
     run_test(
         "import_expected_note_uncommitted",
-        import_expected_note_uncommitted,
+        client::import_expected_note_uncommitted,
         &failed_tests,
         client_config,
     )
     .await;
     run_test(
         "import_expected_notes_from_the_past_as_committed",
-        import_expected_notes_from_the_past_as_committed,
+        client::import_expected_notes_from_the_past_as_committed,
         &failed_tests,
         client_config,
     )
     .await;
-    run_test("get_account_update", get_account_update, &failed_tests, client_config).await;
-    run_test("sync_detail_values", sync_detail_values, &failed_tests, client_config).await;
+    run_test("get_account_update", client::get_account_update, &failed_tests, client_config).await;
+    run_test("sync_detail_values", client::sync_detail_values, &failed_tests, client_config).await;
     run_test(
         "multiple_transactions_can_be_committed_in_different_blocks_without_sync",
-        multiple_transactions_can_be_committed_in_different_blocks_without_sync,
+        client::multiple_transactions_can_be_committed_in_different_blocks_without_sync,
         &failed_tests,
         client_config,
     )
     .await;
     run_test(
         "consume_multiple_expected_notes",
-        consume_multiple_expected_notes,
+        client::consume_multiple_expected_notes,
         &failed_tests,
         client_config,
     )
     .await;
     run_test(
         "import_consumed_note_with_proof",
-        import_consumed_note_with_proof,
+        client::import_consumed_note_with_proof,
         &failed_tests,
         client_config,
     )
     .await;
     run_test(
         "import_consumed_note_with_id",
-        import_consumed_note_with_id,
+        client::import_consumed_note_with_id,
         &failed_tests,
         client_config,
     )
     .await;
-    run_test("import_note_with_proof", import_note_with_proof, &failed_tests, client_config).await;
-    run_test("discarded_transaction", discarded_transaction, &failed_tests, client_config).await;
+    run_test(
+        "import_note_with_proof",
+        client::import_note_with_proof,
+        &failed_tests,
+        client_config,
+    )
+    .await;
+    run_test(
+        "discarded_transaction",
+        client::discarded_transaction,
+        &failed_tests,
+        client_config,
+    )
+    .await;
     run_test(
         "custom_transaction_prover",
-        custom_transaction_prover,
+        client::custom_transaction_prover,
         &failed_tests,
         client_config,
     )
     .await;
-    run_test("locked_account", locked_account, &failed_tests, client_config).await;
+    run_test("locked_account", client::locked_account, &failed_tests, client_config).await;
     run_test(
         "expired_transaction_fails",
-        expired_transaction_fails,
+        client::expired_transaction_fails,
         &failed_tests,
         client_config,
     )
     .await;
-    run_test("unused_rpc_api", unused_rpc_api, &failed_tests, client_config).await;
-    run_test("ignore_invalid_notes", ignore_invalid_notes, &failed_tests, client_config).await;
-    run_test("output_only_note", output_only_note, &failed_tests, client_config).await;
+    run_test("unused_rpc_api", client::unused_rpc_api, &failed_tests, client_config).await;
+    run_test(
+        "ignore_invalid_notes",
+        client::ignore_invalid_notes,
+        &failed_tests,
+        client_config,
+    )
+    .await;
+    run_test("output_only_note", client::output_only_note, &failed_tests, client_config).await;
     // CUSTOM TRANSACTION
-    run_test("merkle_store", merkle_store, &failed_tests, client_config).await;
+    run_test("merkle_store", custom_transaction::merkle_store, &failed_tests, client_config).await;
     run_test(
         "onchain_notes_sync_with_tag",
-        onchain_notes_sync_with_tag,
+        custom_transaction::onchain_notes_sync_with_tag,
         &failed_tests,
         client_config,
     )
     .await;
-    run_test("transaction_request", transaction_request, &failed_tests, client_config).await;
+    run_test(
+        "transaction_request",
+        custom_transaction::transaction_request,
+        &failed_tests,
+        client_config,
+    )
+    .await;
     // FPI
-    run_test("standard_fpi_public", standard_fpi_public, &failed_tests, client_config).await;
-    run_test("standard_fpi_private", standard_fpi_private, &failed_tests, client_config).await;
-    run_test("fpi_execute_program", fpi_execute_program, &failed_tests, client_config).await;
-    run_test("nested_fpi_calls", nested_fpi_calls, &failed_tests, client_config).await;
+    run_test("standard_fpi_public", fpi::standard_fpi_public, &failed_tests, client_config).await;
+    run_test("standard_fpi_private", fpi::standard_fpi_private, &failed_tests, client_config).await;
+    run_test("fpi_execute_program", fpi::fpi_execute_program, &failed_tests, client_config).await;
+    run_test("nested_fpi_calls", fpi::nested_fpi_calls, &failed_tests, client_config).await;
     // NETWORK TRANSACTION
-    run_test("counter_contract_ntx", counter_contract_ntx, &failed_tests, client_config).await;
+    run_test(
+        "counter_contract_ntx",
+        network_transaction::counter_contract_ntx,
+        &failed_tests,
+        client_config,
+    )
+    .await;
     run_test(
         "recall_note_before_ntx_consumes_it",
-        recall_note_before_ntx_consumes_it,
+        network_transaction::recall_note_before_ntx_consumes_it,
         &failed_tests,
         client_config,
     )
     .await;
     // ONCHAIN
-    run_test("import_account_by_id", import_account_by_id, &failed_tests, client_config).await;
-    run_test("onchain_accounts", onchain_accounts, &failed_tests, client_config).await;
-    run_test("onchain_notes_flow", onchain_notes_flow, &failed_tests, client_config).await;
-    run_test("incorrect_genesis", incorrect_genesis, &failed_tests, client_config).await;
+    run_test(
+        "import_account_by_id",
+        onchain::import_account_by_id,
+        &failed_tests,
+        client_config,
+    )
+    .await;
+    run_test("onchain_accounts", onchain::onchain_accounts, &failed_tests, client_config).await;
+    run_test("onchain_notes_flow", onchain::onchain_notes_flow, &failed_tests, client_config).await;
+    run_test("incorrect_genesis", onchain::incorrect_genesis, &failed_tests, client_config).await;
     // SWAP TRANSACTION
-    run_test("swap_fully_onchain", swap_fully_onchain, &failed_tests, client_config).await;
-    run_test("swap_private", swap_private, &failed_tests, client_config).await;
+    run_test(
+        "swap_fully_onchain",
+        swap_transaction::swap_fully_onchain,
+        &failed_tests,
+        client_config,
+    )
+    .await;
+    run_test("swap_private", swap_transaction::swap_private, &failed_tests, client_config).await;
 
     // Print summary
     println!("\n=== TEST SUMMARY ===");
