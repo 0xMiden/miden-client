2965a366eb2a870075edb64a3cd0508e7570b75d
fkrause98 <fkrausear@gmail.com>
2025-08-21 19:13:14 -0300

    pr-comments: docs

--- crates/web-client/js/wasm.js
+++ crates/web-client/js/wasm.js
@@ -1,5 +1,6 @@
 // This is a documented workaround that should avoid issues with Vite projects
 // https://github.com/wasm-tool/rollup-plugin-rust?tab=readme-ov-file#usage-with-vite
+// Also, this effectively disables SSR.
 async function loadWasm() {
   let wasmModule;
   if (!import.meta.env || (import.meta.env && !import.meta.env.SSR)) {
