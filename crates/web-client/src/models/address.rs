8381e4dfd31f360b2bed669cbc9c22b0abc223e0
fkrause98 <fkrausear@gmail.com>
2025-09-02 13:07:24 -0300

    feat: frombech32, toString

--- crates/web-client/src/models/address.rs
+++ crates/web-client/src/models/address.rs
@@ -14,7 +14,6 @@ use super::{
 pub struct Address(NativeAddress);
 
 #[derive(Debug)]
-#[wasm_bindgen(inspectable)]
 pub struct AddressInterface(NativeAddressInterface);
 
 #[wasm_bindgen]
@@ -26,25 +25,40 @@ impl AddressInterface {
             interface.try_into().unwrap(),
         )
     }
+    #[wasm_bindgen(js_name = "toString")]
+    pub fn to_string(&self) -> String {
+        match self.0 {
+            NativeAddressInterface::Unspecified => format!("Unspecified"),
+            NativeAddressInterface::BasicWallet => format!("BasicWallet"),
+        }
+    }
 }
 
 #[wasm_bindgen]
 impl Address {
     #[wasm_bindgen(constructor)]
-    pub fn new(account_id: AccountId, tag_len: u8, interface: AddressInterface) -> Self {
+    pub fn new(account_id: AccountId, interface: AddressInterface) -> Self {
         let address = NativeAccountIdAddress::new(account_id.into(), interface.0);
         Address(NativeAddress::AccountId(address))
     }
     pub fn interface(&self) -> AddressInterface {
         AddressInterface(self.0.interface())
     }
+
     #[wasm_bindgen(js_name = toNoteTag)]
     pub fn to_note_tag(&self) -> NoteTag {
         self.0.to_note_tag().into()
     }
+
     // FIXME: Handle errors
     pub fn to_bech32(&self, network_id: NetworkId) -> String {
         let net_id = network_id.try_into().unwrap();
         self.0.to_bech32(net_id)
+    }
+
+    // FIXME: Handle errors
+    pub fn from_bech32(bech32: &str) -> Self {
+        let (_net_id, address) = NativeAddress::from_bech32(bech32).unwrap();
+        Self(address)
     }
 }
