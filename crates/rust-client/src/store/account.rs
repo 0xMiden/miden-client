28c0adc43e8adb60fd6a47aa4ff98f3c4ac85fd3
juan518munoz <62400508+juan518munoz@users.noreply.github.com>
2025-09-18 09:00:17 -0300

    Update crates/rust-client/src/store/account.rs
    Co-authored-by: Tomas <tomas.arrachea@lambdaclass.com>

--- crates/rust-client/src/store/account.rs
+++ crates/rust-client/src/store/account.rs
@@ -18,7 +18,7 @@ pub struct AccountRecord {
     account: Account,
     /// Status of the tracked account.
     status: AccountStatus,
-    /// Addresses that the account can be refereded as.
+    /// Addresses that the account can be referred as.
     addresses: Vec<AccountIdAddress>,
 }
 
