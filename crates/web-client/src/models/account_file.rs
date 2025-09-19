054f900d406d6eac462cde1ebc6a05a82eeb6bf5
Ngalim Siregar <ngalim.siregar@gmail.com>
2025-09-19 13:39:42 +0700

    update method names and enable Debug, Clone trait

--- crates/web-client/src/models/account_file.rs
+++ crates/web-client/src/models/account_file.rs
@@ -4,21 +4,19 @@ use wasm_bindgen_futures::js_sys::Uint8Array;
 
 use crate::utils::{deserialize_from_uint8array, serialize_to_uint8array};
 
-// #[derive(Debug, Clone)]
+#[derive(Debug, Clone)]
 #[wasm_bindgen]
 pub struct AccountFile(NativeAccountFile);
 
 #[wasm_bindgen]
 impl AccountFile {
     /// Serializes the `AccountFile` into a byte array
-    #[wasm_bindgen(js_name = "write_bytes")]
-    pub fn write_bytes(&self) -> Uint8Array {
+    pub fn serialize(&self) -> Uint8Array {
         serialize_to_uint8array(&self.0)
     }
 
     /// Deserializes a byte array into an `AccountFile`
-    #[wasm_bindgen(js_name = "read_from_bytes")]
-    pub fn read_from_bytes(bytes: &Uint8Array) -> Result<AccountFile, JsValue> {
+    pub fn deserialize(bytes: &Uint8Array) -> Result<AccountFile, JsValue> {
         let native_account_file: NativeAccountFile = deserialize_from_uint8array(bytes)?;
         Ok(Self(native_account_file))
     }
