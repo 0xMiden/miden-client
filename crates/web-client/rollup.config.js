6d71142c7a60f7d8d442d681458ea02bdbbbf668
Ignacio Amigo <ignacio.amigo@lambdaclass.com>
2025-09-30 14:13:09 -0300

    refactor: remove need for nightly instead

--- crates/web-client/rollup.config.js
+++ crates/web-client/rollup.config.js
@@ -17,7 +17,7 @@ const baseCargoArgs = [
   "--features",
   "testing",
   "--config",
-  `build.rustflags=["-C", "target-feature=+atomics,+bulk-memory,+mutable-globals", "-C", "link-arg=--max-memory=4294967296", "-Z", "unstable-options", "-C", "panic=immediate-abort"]`,
+  `build.rustflags=["-C", "target-feature=+atomics,+bulk-memory,+mutable-globals", "-C", "link-arg=--max-memory=4294967296", "-C", "panic=abort"]`,
   "--no-default-features",
 ];
 
