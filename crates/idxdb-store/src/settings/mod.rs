a8a918c9f9297c2a42f22080905043d6400cc673
Tomas <tomas.arrachea@lambdaclass.com>
2025-10-02 11:50:16 -0300

    refactor: use webstore utils (#1358)
    * feat: add migrations and settings tables
    
    * feat: add settings table to web store
    
    * chore: lint
    
    * docs: update web-client docs
    
    * review: use Uint8Array instead of blob
    
    * review: refactor account cmd
    
    * chore: bump web client version
    
    * chore: lint
    
    * review: address comments
    
    * review: add method to list settings keys
    
    * merge: add comment
    
    * feat: add listKeys
    
    * chore: bump sdk version
    
    * chore: fix merge
    
    * review: rename settings accessors
    
    * review: rename settings accessors
    
    * chore: bump sdk version
    
    * chore: bump sdk version
    
    * review: add doc comments
    
    * review: docs
    
    * feat: refactor web store to reuse utils
    
    * chore: lint
    
    * feat: refactor more code to reuse utls
    
    * chore: fmt
    
    * review: import utils
    
    * feat: add comment
    
    ---------
    
    Co-authored-by: igamigo <ignacio.amigo@lambdaclass.com>

--- crates/idxdb-store/src/settings/mod.rs
+++ crates/idxdb-store/src/settings/mod.rs
@@ -2,8 +2,6 @@ use alloc::string::String;
 use alloc::vec::Vec;
 
 use miden_client::store::StoreError;
-use serde_wasm_bindgen::from_value;
-use wasm_bindgen_futures::JsFuture;
 
 mod js_bindings;
 mod models;
@@ -16,46 +14,32 @@ use js_bindings::{
 };
 
 use crate::WebStore;
+use crate::promise::{await_js, await_js_value};
 use crate::settings::models::SettingValueIdxdbObject;
 
 impl WebStore {
     pub(crate) async fn set_setting(&self, key: String, value: Vec<u8>) -> Result<(), StoreError> {
         let promise = idxdb_insert_setting(key, value);
-        JsFuture::from(promise).await.map_err(|js_error| {
-            StoreError::DatabaseError(format!("failed to set setting value: {js_error:?}",))
-        })?;
+        await_js_value(promise, "failed to set setting value").await?;
         Ok(())
     }
 
     pub(crate) async fn get_setting(&self, key: String) -> Result<Option<Vec<u8>>, StoreError> {
         let promise = idxdb_get_setting(key);
-        let value = JsFuture::from(promise).await.map_err(|js_error| {
-            StoreError::DatabaseError(format!(
-                "failed to get setting value from idxdb: {js_error:?}",
-            ))
-        })?;
-        let setting: Option<SettingValueIdxdbObject> = from_value(value).map_err(|err| {
-            StoreError::DatabaseError(format!("failed to deserialize value from idxdb: {err:?}"))
-        })?;
+        let setting: Option<SettingValueIdxdbObject> =
+            await_js(promise, "failed to get setting value from idxdb").await?;
         Ok(setting.map(|setting| setting.value))
     }
 
     pub(crate) async fn remove_setting(&self, key: String) -> Result<(), StoreError> {
         let promise = idxdb_remove_setting(key);
-        JsFuture::from(promise).await.map_err(|js_error| {
-            StoreError::DatabaseError(format!("failed to delete setting value: {js_error:?}",))
-        })?;
+        await_js_value(promise, "failed to delete setting value").await?;
         Ok(())
     }
 
     pub(crate) async fn list_setting_keys(&self) -> Result<Vec<String>, StoreError> {
         let promise = idxdb_list_setting_keys();
-        let keys = JsFuture::from(promise).await.map_err(|js_error| {
-            StoreError::DatabaseError(format!("failed to list setting keys: {js_error:?}",))
-        })?;
-        let keys: Vec<String> = from_value(keys).map_err(|err| {
-            StoreError::DatabaseError(format!("failed to deserialize setting keys: {err:?}"))
-        })?;
+        let keys: Vec<String> = await_js(promise, "failed to list setting keys").await?;
         Ok(keys)
     }
 }
