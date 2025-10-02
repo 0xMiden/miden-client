7d91e9fe02dfb01421befa7c247098b1d7af9c71
Tomas <tomas.arrachea@lambdaclass.com>
2025-10-02 05:31:03 -0300

    feat: webstore settings table (#1294)
    * feat: add migrations and settings tables
    
    * feat: add settings table to web store
    
    * chore: lint
    
    * docs: update web-client docs
    
    * review: use Uint8Array instead of blob
    
    * review: refactor account cmd
    
    * chore: bump web client version
    
    * chore: lint
    
    * review: address comments
    
    * review: add method to list settings keys
    
    * merge: add comment
    
    * feat: add listKeys
    
    * chore: bump sdk version
    
    * chore: fix merge
    
    * review: rename settings accessors
    
    * review: rename settings accessors
    
    * chore: bump sdk version
    
    * chore: bump sdk version
    
    * review: add doc comments
    
    * review: docs
    
    * Update crates/web-client/package.json
    
    ---------
    
    Co-authored-by: igamigo <ignacio.amigo@lambdaclass.com>
    Co-authored-by: Marti <marti@miden.team>

---
+++ crates/idxdb-store/src/settings/models.rs
@@ -0,0 +1,25 @@
+use alloc::string::String;
+use alloc::vec::Vec;
+
+use base64::Engine as _;
+use base64::engine::general_purpose;
+use serde::de::Error;
+use serde::{Deserialize, Deserializer, Serialize};
+
+#[derive(Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct SettingValueIdxdbObject {
+    pub key: String,
+    #[serde(deserialize_with = "base64_to_vec_u8_required", default)]
+    pub value: Vec<u8>,
+}
+
+fn base64_to_vec_u8_required<'de, D>(deserializer: D) -> Result<Vec<u8>, D::Error>
+where
+    D: Deserializer<'de>,
+{
+    let base64_str: String = Deserialize::deserialize(deserializer)?;
+    general_purpose::STANDARD
+        .decode(&base64_str)
+        .map_err(|e| Error::custom(format!("Base64 decode error: {e}")))
+}
