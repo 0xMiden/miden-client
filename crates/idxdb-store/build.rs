a73bc040bc7b4f1e2892abfece4759f903a00325
fkrause98 <fkrausear@gmail.com>
2025-10-20 17:32:42 -0300

    format: cargo.toml, build.rs, package.json

--- crates/idxdb-store/build.rs
+++ crates/idxdb-store/build.rs
@@ -3,7 +3,7 @@ use std::process::Command;
 fn main() {
     println!("cargo::rerun-if-changed=src/ts");
     Command::new("yarn")
-        .args(&["build"])
+        .args(["build"])
         .current_dir("./src")
         .status()
         .expect("failed to build typescript files");
