90c7372d531d871f344aef307d304016f81c82b9
fkrause98 <fkrausear@gmail.com>
2025-10-21 16:09:03 -0300

    feat: rewrite build script
    feat: rewrite build script

--- crates/idxdb-store/build.rs
+++ crates/idxdb-store/build.rs
@@ -1,10 +1,19 @@
 use std::process::Command;
 
-fn main() {
-    println!("cargo::rerun-if-changed=src/ts");
+fn main() -> miette::Result<(), String> {
+    println!("cargo::rerun-if-changed=.");
+
+    // Install dependencies for the TS project files with yarn
+    Command::new("yarn").current_dir("src").status().map_err(|err| {
+        format!("could not install ts dependencies -- have you installed yarn? got error: {err}")
+    })?;
+
+    // Build the TS files into JS and store the artifacts under src/js
     Command::new("yarn")
         .args(["build"])
-        .current_dir("./src")
+        .current_dir("src")
         .status()
-        .expect("failed to build typescript files");
+        .map_err(|err| format!("failed to run build command for typescript: {err}"))?;
+
+    Ok(())
 }
