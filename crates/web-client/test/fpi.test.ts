a0791baeee9e1ef7880e7d48d9d95d667fa162d5
Francisco Krause Arnim <fkrausear@gmail.com>
2025-11-12 16:16:52 -0300

    pr: comments, remove prints in tests

--- crates/web-client/test/fpi.test.ts
+++ crates/web-client/test/fpi.test.ts
@@ -3,14 +3,12 @@ import test from "./playwright.global.setup";
 
 export const testStandardFpi = async (page: Page): Promise<void> => {
   return await page.evaluate(async () => {
-    console.log("--- INIT TEST ---");
     const client = window.client;
     await client.syncState();
 
     // BUILD FOREIGN ACCOUNT WITH CUSTOM COMPONENT
     // --------------------------------------------------------------------------
 
-    console.log("--- FELTS ---");
     let felt1 = new window.Felt(15n);
     let felt2 = new window.Felt(15n);
     let felt3 = new window.Felt(15n);
@@ -20,7 +18,6 @@ export const testStandardFpi = async (page: Page): Promise<void> => {
       new BigUint64Array([9n, 12n, 18n, 30n])
     );
 
-    console.log("--- MAP ---");
     let storageMap = new window.StorageMap();
     storageMap.insert(MAP_KEY, FPI_STORAGE_VALUE);
 
@@ -39,29 +36,19 @@ export const testStandardFpi = async (page: Page): Promise<void> => {
       window.StorageSlot.map(storageMap),
     ]).withSupportsAllTypes();
 
-    console.log("--- SEED ---");
     const walletSeed = new Uint8Array(32);
     crypto.getRandomValues(walletSeed);
 
-    console.log("--- Secret Key ---");
     let secretKey = window.SecretKey.rpoFalconWithRNG(walletSeed);
-    console.log("GENERATED SEED: ", JSON.stringify(walletSeed));
+
     let authComponent = window.AccountComponent.createAuthComponent(secretKey);
 
-    console.log("--- GET ITEM ACCOUNT BUILDER ---");
     let getItemAccountBuilderResult = new window.AccountBuilder(walletSeed)
       .withAuthComponent(authComponent)
       .withComponent(getItemComponent)
       .storageMode(window.AccountStorageMode.public())
       .build();
 
-    console.log(
-      "BUILT ACCOUNT",
-      getItemAccountBuilderResult.account.id().toString()
-    );
-    console.log("BUILT SEED", getItemAccountBuilderResult.seed.toHex());
-
-    console.log("--- PROCEDURE HASH ---");
     let getFpiMapItemProcedureHash =
       getItemComponent.getProcedureHash("get_fpi_map_item");
 
@@ -70,21 +57,17 @@ export const testStandardFpi = async (page: Page): Promise<void> => {
 
     let foreignAccountId = getItemAccountBuilderResult.account.id();
 
-    console.log("THE FOREIGN ACCOUNT ID: ", foreignAccountId.toString());
-
     await client.addAccountSecretKeyToWebStore(secretKey);
     await client.newAccount(getItemAccountBuilderResult.account, false);
     await client.syncState();
 
     let txRequest = new window.TransactionRequestBuilder().build();
 
-    console.log("--- EXECUTE AND APPLY ---");
     let txResult = await window.helpers.executeAndApplyTransaction(
       foreignAccountId,
       txRequest
     );
 
-    console.log("--- EXECUTED ID  ---");
     let txId = txResult.executedTransaction().id();
 
     await window.helpers.waitForTransaction(txId.toHex());
@@ -112,7 +95,7 @@ export const testStandardFpi = async (page: Page): Promise<void> => {
                 push.9.12.18.30 assert_eqw
             end
         `;
-    console.log("--- TX SCRIPT ---");
+
     txScript = txScript
       .replace("{proc_root}", getFpiMapItemProcedureHash)
       .replace("{account_id_suffix}", foreignAccountId.suffix().toString())
@@ -121,14 +104,11 @@ export const testStandardFpi = async (page: Page): Promise<void> => {
         foreignAccountId.prefix().asInt().toString()
       );
 
-    console.log("--- COMPILED TX SCRIPT ---");
     let compiledTxScript = builder.compileTxScript(txScript);
 
     await client.syncState();
 
     await window.helpers.waitForBlocks(2);
-
-    console.log("--- WAITED FOR BLOCKS---");
 
     let slotAndKeys = new window.SlotAndKeys(1, [MAP_KEY]);
     let storageRequirements =
@@ -146,14 +126,10 @@ export const testStandardFpi = async (page: Page): Promise<void> => {
       )
       .build();
 
-    console.log("--- BEFORE TX RESULT 2---");
-
     let txResult2 = await window.helpers.executeAndApplyTransaction(
       newAccount.id(),
       txRequest2
     );
-
-    console.log("--- TX RESULT 2 APPLIED---");
   });
 };
 
