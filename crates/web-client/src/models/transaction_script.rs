2d95c4c2d4b0c6579b3cf39aca6b05bb9f29da89
Juan Munoz <juanmunoz890@gmail.com>
2025-11-25 14:56:26 -0300

    feat: add `fromPackage` for `NoteScript` & `TransactionScript`

--- crates/web-client/src/models/transaction_script.rs
+++ crates/web-client/src/models/transaction_script.rs
@@ -1,6 +1,8 @@
 use miden_client::transaction::TransactionScript as NativeTransactionScript;
+use miden_client::vm::Package as NativePackage;
 use wasm_bindgen::prelude::*;
 
+use crate::models::package::Package;
 use crate::models::word::Word;
 
 #[derive(Clone)]
@@ -11,6 +13,20 @@ pub struct TransactionScript(NativeTransactionScript);
 impl TransactionScript {
     pub fn root(&self) -> Word {
         self.0.root().into()
+    }
+
+    /// Builds a `TransactionScript` from a `Package`.
+    #[wasm_bindgen(js_name = "fromPackage")]
+    pub fn from_package(package: Package) -> Result<TransactionScript, JsValue> {
+        let native_package: NativePackage = package.into();
+
+        if !native_package.is_program() {
+            return Err(JsValue::from_str("Package is not a program"));
+        }
+
+        let program = native_package.unwrap_program();
+        let native_transaction_script = NativeTransactionScript::new((*program).clone());
+        Ok(native_transaction_script.into())
     }
 }
 
