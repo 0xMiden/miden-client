114f3fa5c8a3c926c1e38e129252bef62a84b9b7
Wiktor Starczewski <poszerny@gmail.com>
2025-11-26 16:58:28 +0100

    Move to TryFrom impl on auth_scheme

--- crates/web-client/src/models/auth_scheme.rs
+++ crates/web-client/src/models/auth_scheme.rs
@@ -1,3 +1,6 @@
+use core::convert::TryFrom;
+use core::fmt::Debug;
+
 use miden_objects::account::auth::AuthScheme as NativeAuthScheme;
 use wasm_bindgen::prelude::*;
 
@@ -10,26 +13,29 @@ pub enum AuthScheme {
     AuthEcdsaK256Keccak = 1,
 }
 
-const _: () = {
-    assert!(NativeAuthScheme::RpoFalcon512 as u8 == AuthScheme::AuthRpoFalcon512 as u8);
-    assert!(NativeAuthScheme::EcdsaK256Keccak as u8 == AuthScheme::AuthEcdsaK256Keccak as u8);
-};
+impl TryFrom<AuthScheme> for NativeAuthScheme {
+    type Error = JsValue;
 
-impl From<AuthScheme> for NativeAuthScheme {
-    fn from(value: AuthScheme) -> Self {
+    fn try_from(value: AuthScheme) -> Result<Self, Self::Error> {
         match value {
-            AuthScheme::AuthRpoFalcon512 => NativeAuthScheme::RpoFalcon512,
-            AuthScheme::AuthEcdsaK256Keccak => NativeAuthScheme::EcdsaK256Keccak,
+            AuthScheme::AuthRpoFalcon512 => Ok(NativeAuthScheme::RpoFalcon512),
+            AuthScheme::AuthEcdsaK256Keccak => Ok(NativeAuthScheme::EcdsaK256Keccak),
         }
     }
 }
 
-impl From<NativeAuthScheme> for AuthScheme {
-    fn from(value: NativeAuthScheme) -> Self {
+impl TryFrom<NativeAuthScheme> for AuthScheme {
+    type Error = JsValue;
+
+    fn try_from(value: NativeAuthScheme) -> Result<Self, Self::Error> {
         match value {
-            NativeAuthScheme::RpoFalcon512 => AuthScheme::AuthRpoFalcon512,
-            NativeAuthScheme::EcdsaK256Keccak => AuthScheme::AuthEcdsaK256Keccak,
-            _ => unreachable!("unsupported auth scheme: {value:?}"),
+            NativeAuthScheme::RpoFalcon512 => Ok(AuthScheme::AuthRpoFalcon512),
+            NativeAuthScheme::EcdsaK256Keccak => Ok(AuthScheme::AuthEcdsaK256Keccak),
+            _ => Err(unsupported_scheme_error(value)),
         }
     }
+}
+
+fn unsupported_scheme_error(scheme: impl Debug) -> JsValue {
+    JsValue::from_str(&format!("unsupported auth scheme: {scheme:?}"))
 }
