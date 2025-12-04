94988fbb1e41e33ae185eb7d62106b095d197b47
Wiktor Starczewski <poszerny@gmail.com>
2025-12-04 12:04:44 +0100

    Detect runtime env in util

--- crates/idxdb-store/src/ts/utils.ts
+++ crates/idxdb-store/src/ts/utils.ts
@@ -1,4 +1,11 @@
 import Dexie from "dexie";
+
+export const isNodeRuntime = () =>
+  typeof process !== "undefined" && !!process.versions?.node;
+
+export const isBrowserRuntime = () =>
+  typeof window !== "undefined" && typeof window.document !== "undefined";
+
 // Helper for undefined values, like map for Option<T> in Rust.
 // A better name for this is welcome.
 export const mapOption = <T, U>(
@@ -8,11 +15,20 @@ export const mapOption = <T, U>(
   return value != undefined ? func(value) : undefined;
 };
 
+const isDexieError = (
+  error: unknown
+): error is InstanceType<typeof Dexie.DexieError> & {
+  inner?: InstanceType<typeof Dexie.DexieError>;
+} =>
+  typeof Dexie !== "undefined" &&
+  typeof indexedDB !== "undefined" &&
+  error instanceof Dexie.DexieError;
+
 // Anything can be thrown as an error in raw JS (also the TS compiler can't type-check exceptions),
 // so we allow it here.
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const logWebStoreError = (error: any, errorContext?: string) => {
-  if (error instanceof Dexie.DexieError) {
+  if (isDexieError(error)) {
     if (errorContext) {
       console.error(
         `${errorContext}: Indexdb error (${error.name}): ${error.message}`
@@ -28,7 +44,7 @@ export const logWebStoreError = (error: any, errorContext?: string) => {
     );
   } else if (error instanceof Error) {
     console.error(
-      `Unexpected error while accessing indexdb: ${error.toString()}`
+      `Unexpected error while accessing persistent store: ${error.toString()}`
     );
     mapOption(error.stack, (stack) => {
       console.error(`Stacktrace: ${stack}`);
@@ -43,6 +59,9 @@ export const logWebStoreError = (error: any, errorContext?: string) => {
 };
 
 export const uint8ArrayToBase64 = (bytes: Uint8Array) => {
+  if (typeof Buffer !== "undefined") {
+    return Buffer.from(bytes).toString("base64");
+  }
   const binary = bytes.reduce(
     (acc, byte) => acc + String.fromCharCode(byte),
     ""
