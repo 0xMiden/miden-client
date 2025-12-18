c1d41faaffc24a0c7e180b2812dc67cb69c94f85
SantiagoPittella <pittellasantiago@gmail.com>
2025-12-18 11:23:34 -0300

    fix: reduce notes limits to 100

--- crates/rust-client/src/rpc/mod.rs
+++ crates/rust-client/src/rpc/mod.rs
@@ -97,7 +97,7 @@ pub enum AccountStateAt {
 // ================================================================================================
 
 // TODO: We need a better structured way of getting limits as defined by the node (#1139)
-pub const NOTE_IDS_LIMIT: usize = 1000;
+pub const NOTE_IDS_LIMIT: usize = 100;
 pub const NULLIFIER_PREFIXES_LIMIT: usize = 1000;
 pub const ACCOUNT_ID_LIMIT: usize = 1000;
 pub const NOTE_TAG_LIMIT: usize = 1000;
