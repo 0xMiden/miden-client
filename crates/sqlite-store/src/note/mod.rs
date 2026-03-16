898e87b238f8e4c54a7e85b25bc94819b4f1c760
tomasarrachea <tomas.arrachea@lambdaclass.com>
2026-03-16 12:27:24 -0300

    feat: remove unused

--- crates/sqlite-store/src/note/mod.rs
+++ crates/sqlite-store/src/note/mod.rs
@@ -282,54 +282,6 @@ pub(super) fn upsert_input_note_tx(
     Ok(())
 }
 
-/// Inserts the provided output note into the database, if the note already exists, it will be
-/// replaced.
-#[allow(dead_code)]
-pub fn upsert_output_note_tx(
-    tx: &Transaction<'_>,
-    note: &OutputNoteRecord,
-) -> Result<(), StoreError> {
-    const NOTE_QUERY: &str = insert_sql!(
-        output_notes {
-            note_id,
-            assets,
-            recipient_digest,
-            metadata,
-            nullifier,
-            expected_height,
-            state_discriminant,
-            state
-        } | REPLACE
-    );
-
-    let SerializedOutputNoteData {
-        id,
-        assets,
-        metadata,
-        nullifier,
-        recipient_digest,
-        expected_height,
-        state_discriminant,
-        state,
-    } = serialize_output_note(note);
-
-    tx.prepare_cached(NOTE_QUERY)
-        .into_store_error()?
-        .execute(params![
-            id,
-            assets,
-            recipient_digest,
-            metadata,
-            nullifier,
-            expected_height,
-            state_discriminant,
-            state,
-        ])
-        .into_store_error()?;
-
-    Ok(())
-}
-
 /// Parse input note columns from the provided row into native types.
 fn parse_input_note_columns(
     row: &rusqlite::Row<'_>,
